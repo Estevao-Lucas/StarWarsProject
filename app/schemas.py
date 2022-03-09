01d00e88b953f063340cfb2ce377f4757efdc8ba
Estevão Lucas <3stevaolucas@gmail.com>
2022-03-09 14:15:56 -0300

    schemas

---
+++ app/schemas.py
@@ -0,0 +1,49 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, validator
+
+from .security import get_password_hash
+
+class FavCharacterBase(BaseModel):
+    name: str
+    height: str
+    mass: str
+    skin_color: str
+    eye_color: str
+    gender: str
+
+class FavCharacterCreate(FavCharacterBase):
+    pass
+
+class FavCharacter(FavCharacterBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    username: str
+    email: Optional[str] = None
+
+class UserCreate(UserBase):
+    password: str
+
+    @validator('password', pre=True)
+    def has_the_password(cls, v):
+        return get_password_hash(v)
+
+class User(UserBase):
+    id: int
+    favorite_char: List[FavCharacter] = []
+
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str
+
