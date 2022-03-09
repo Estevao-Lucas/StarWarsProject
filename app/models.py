a4beee35c3c288a0abce764f1f3ad8641ef4411d
Estevão Lucas <3stevaolucas@gmail.com>
2022-03-09 13:38:30 -0300

    Modelo tabela db

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    favorite_char = relationship("Character", back_populates="owner")
+
+class Character(Base):
+    __tablename__ = "character"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    height = Column(String, index=True)
+    mass = Column(String, index=True)
+    skin_color = Column(String, index=True)
+    eye_color = Column(String, index=True)
+    gender = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", backref="character")
