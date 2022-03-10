2d1529cdd8d66f886da1ebca7f9009a4bbda90a3
Estevão Lucas <3stevaolucas@gmail.com>
2022-03-10 20:26:26 -0300

    Alteração no modelo

--- app/models.py
+++ app/models.py
@@ -19,8 +19,10 @@ class Character(Base):
     name = Column(String, index=True)
     height = Column(String, index=True)
     mass = Column(String, index=True)
+    hair_color = Column(String, index=True)
     skin_color = Column(String, index=True)
     eye_color = Column(String, index=True)
+    birth_year = Column(String, index=True)
     gender = Column(String, index=True)
     owner_id = Column(ForeignKey("users.id"))
 
--- app/schemas.py
+++ app/schemas.py
@@ -8,8 +8,10 @@ class FavCharacterBase(BaseModel):
     name: str
     height: str
     mass: str
+    hair_color: str
     skin_color: str
     eye_color: str
+    birth_year: str
     gender: str
 
 class FavCharacterCreate(FavCharacterBase):
